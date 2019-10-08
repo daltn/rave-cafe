d0f9886e37553642f3e3dc30e2a4683c2a9280f5
Dalton Saffe <daltonsaffe@gmail.com>
2019-10-07 23:42:21 -0400

    add rave cafe title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [hover, setHover] = useState(false)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1
+        onMouseOver={() => setHover(!hover)}
+        className={hover ? "spin blur" : "spin"}
+      >
+        rave.cafe
+      </h1>
+    </Layout>
+  )
+}
 
 export default IndexPage
